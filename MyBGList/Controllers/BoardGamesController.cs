a7c72f81d915f4b7ba9494f684c6a4a26f8a40c8
Dmitry Sirotkin <dmitriy.sirotkin@finstek.com>
2024-01-16 06:42:23 +0300

    Add CORS and REST models

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBGList.DTO;
 
 namespace MyBGList.Controllers
 {
@@ -14,30 +15,42 @@ namespace MyBGList.Controllers
 		}
 
 		[HttpGet(Name = "GetBoardGames")]
-		public IEnumerable<BoardGame> Get()
+		[ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
+		public RestDTO<BoardGame[]> Get()
 		{
-			return new[] {
-				new BoardGame() {
-					Id = 1,
-					Name = "Axis & Allies",
-					Year = 1981,
-					MinPlayers = 2,
-					MaxPlayers = 5
-				},
-				new BoardGame() {
-					Id = 2,
-					Name = "Citadels",
-					Year = 2000,
-					MinPlayers = 2,
-					MaxPlayers = 8
-				},
-				new BoardGame() {
-					Id = 3,
-					Name = "Terraforming Mars",
-					Year = 2016,
-					MinPlayers = 1,
-					MaxPlayers = 5
-				}
+			return new RestDTO<BoardGame[]>
+			{
+				Data = [
+					new BoardGame()
+					{
+						Id = 1,
+						Name = "Axis & Allies",
+						Year = 1981,
+						MinPlayers = 2,
+						MaxPlayers = 5
+					},
+					new BoardGame()
+					{
+						Id = 2,
+						Name = "Citadels",
+						Year = 2000,
+						MinPlayers = 2,
+						MaxPlayers = 8
+					},
+					new BoardGame()
+					{
+						Id = 3,
+						Name = "Terraforming Mars",
+						Year = 2016,
+						MinPlayers = 1,
+						MaxPlayers = 5
+					}],
+				Links = [
+					new LinkDTO(
+						Url.Action(null, "BoardGames", null, Request.Scheme)!,
+						"self",
+						"GET")
+				]
 			};
 		}
 	}
--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(cfg =>
+	{
+		cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+		cfg.AllowAnyHeader();
+		cfg.AllowAnyMethod();
+	});
+	options.AddPolicy(name: "AnyOrigin", cfg =>
+	{
+		cfg.AllowAnyOrigin();
+		cfg.AllowAnyHeader();
+		cfg.AllowAnyMethod();
+	});
+});
 
 var app = builder.Build();
 
@@ -23,10 +42,31 @@ else
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthorization();
 
-app.MapGet("/error", () => Results.Problem());
-app.MapGet("/error/test", () => { throw new Exception("test"); });
+app.MapGet("/error",
+	[EnableCors("AnyOrigin")]
+	[ResponseCache(NoStore = true)] () =>
+	Results.Problem());
+app.MapGet("/error/test",
+	[EnableCors("AnyOrigin")]
+	[ResponseCache(NoStore = true)] () =>
+	{ throw new Exception("test"); })
+	.RequireCors("AnyOrigin");
+app.MapGet("/cod/test",
+	[EnableCors("AnyOrigin")]
+	[ResponseCache(NoStore = true)] () =>
+	Results.Text("<script>" +
+		"window.alert('Your client supports JavaScript!" +
+		"\\r\\n\\r\\n" +
+		$"Server time (UTC): {DateTime.UtcNow:o}" +
+		"\\r\\n" +
+		"Client time (UTC): ' + new Date().toISOString());" +
+		"</script>" +
+		"<noscript>Your client does not support JavaScript</noscript>",
+		"text/html"));
 
 app.MapControllers();
 
