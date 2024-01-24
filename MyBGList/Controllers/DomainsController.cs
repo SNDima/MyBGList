2e177441f23ffb68cff93b5f5495a9069ea8adc6
Dmitry Sirotkin <dmitriy.sirotkin@finstek.com>
2024-01-24 09:18:37 +0300

    Add manual validation processing and some exception handling technicks.

--- MyBGList/Controllers/DomainsController.cs
+++ MyBGList/Controllers/DomainsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MyBGList.Attributes;
 using MyBGList.DTO;
 using MyBGList.Models;
 using System.Linq.Dynamic.Core;
@@ -22,9 +23,35 @@ namespace MyBGList.Controllers
 
 		[HttpGet(Name = "GetDomains")]
 		[ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
-		public async Task<RestDTO<Domain[]>> Get(
+		[ManualValidationFilter]
+		public async Task<ActionResult<RestDTO<Domain[]>>> Get(
 			[FromQuery] RequestDTO<DomainDTO> input)
 		{
+			if (!ModelState.IsValid)
+			{
+				var details = new ValidationProblemDetails(ModelState);
+				details.Extensions["traceId"] =
+					System.Diagnostics.Activity.Current?.Id
+					  ?? HttpContext.TraceIdentifier;
+				if (ModelState.Keys.Any(k => k == "PageSize"))
+				{
+					details.Type =
+						"https://tools.ietf.org/html/rfc7231#section-6.6.2";
+					details.Status = StatusCodes.Status501NotImplemented;
+					return new ObjectResult(details)
+					{
+						StatusCode = StatusCodes.Status501NotImplemented
+					};
+				}
+				else
+				{
+					details.Type =
+						"https://tools.ietf.org/html/rfc7231#section-6.5.1";
+					details.Status = StatusCodes.Status400BadRequest;
+					return new BadRequestObjectResult(details);
+				}
+			}
+
 			var query = _context.Domains.AsQueryable();
 
 			if (!string.IsNullOrEmpty(input.FilterQuery))
--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyBGList.Models;
@@ -8,7 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+	options.ModelBindingMessageProvider.SetValueIsInvalidAccessor(
+		(x) => $"The value '{x}' is invalid.");
+	options.ModelBindingMessageProvider.SetValueMustBeANumberAccessor(
+		(x) => $"The field {x} must be a number.");
+	options.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor(
+		(x, y) => $"The value '{x}' is not valid for {y}.");
+	options.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(
+		() => $"A value is required.");
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -38,6 +49,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 		builder.Configuration.GetConnectionString("DefaultConnection"))
 	);
 
+// Code replaced by the [ManualValidationFilter] attribute
+// builder.Services.Configure<ApiBehaviorOptions>(options =>
+//	 options.SuppressModelStateInvalidFilter = true);
 
 var app = builder.Build();
 
@@ -52,6 +66,24 @@ if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
 	app.UseDeveloperExceptionPage();
 else
 	app.UseExceptionHandler("/error");
+//app.UseExceptionHandler(action => {
+//    action.Run(async context =>
+//    {
+//        var exceptionHandler =
+//            context.Features.Get<IExceptionHandlerPathFeature>();
+
+//        var details = new ProblemDetails();
+//        details.Detail = exceptionHandler?.Error.Message;
+//        details.Extensions["traceId"] =
+//            System.Diagnostics.Activity.Current?.Id 
+//              ?? context.TraceIdentifier;
+//        details.Type =
+//            "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+//        details.Status = StatusCodes.Status500InternalServerError;
+//        await context.Response.WriteAsync(
+//            System.Text.Json.JsonSerializer.Serialize(details));
+//    });
+//});
 
 app.UseHttpsRedirection();
 
@@ -61,13 +93,28 @@ app.UseAuthorization();
 
 app.MapGet("/error",
 	[EnableCors("AnyOrigin")]
-[ResponseCache(NoStore = true)] () =>
-	Results.Problem());
+[ResponseCache(NoStore = true)] (HttpContext context) =>
+	{
+		var exceptionHandler =
+			context.Features.Get<IExceptionHandlerPathFeature>();
+
+		var details = new ProblemDetails();
+		details.Detail = exceptionHandler?.Error.Message;
+		details.Extensions["traceId"] =
+			System.Diagnostics.Activity.Current?.Id
+			  ?? context.TraceIdentifier;
+		details.Type =
+			"https://tools.ietf.org/html/rfc7231#section-6.6.1";
+		details.Status = StatusCodes.Status500InternalServerError;
+		return Results.Problem(details);
+	});
+
 app.MapGet("/error/test",
 	[EnableCors("AnyOrigin")]
 [ResponseCache(NoStore = true)] () =>
-	{ throw new Exception("test"); })
+		{ throw new Exception("test"); })
 	.RequireCors("AnyOrigin");
+
 app.MapGet("/cod/test",
 	[EnableCors("AnyOrigin")]
 [ResponseCache(NoStore = true)] () =>
