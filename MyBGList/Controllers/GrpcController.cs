73affc1a33ee1bc611d1a3a2c87819bff6f8f6ac
dmitry.sirotkin <d.sirotkin.n@gmail.com>
2024-02-28 20:27:46 +0800

    Implement exercises.

--- MyBGList/Controllers/GrpcController.cs
+++ MyBGList/Controllers/GrpcController.cs
@@ -35,5 +35,59 @@ namespace MyBGList.Controllers
 
             return response;
         }
+
+        [HttpGet("{id}")]
+        public async Task<DomainResponse> GetDomain(int id)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:40443");
+            var client = new gRPC.Grpc.GrpcClient(channel);
+            var response = await client.GetDomainAsync(new DomainRequest { Id = id });
+            return response;
+        }
+
+        [HttpPost]
+        public async Task<DomainResponse> UpdateDomain(
+            string token, int id, string name)
+        {
+            var headers = new Metadata
+            {
+                { "Authorization", $"Bearer {token}" }
+            };
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:40443");
+            var client = new gRPC.Grpc.GrpcClient(channel);
+            var response = await client.UpdateDomainAsync(
+                new UpdateDomainRequest { Id = id, Name = name },
+                headers);
+
+            return response;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<MechanicResponse> GetMechanic(int id)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:40443");
+            var client = new gRPC.Grpc.GrpcClient(channel);
+            var response = await client.GetMechanicAsync(new MechanicRequest { Id = id });
+            return response;
+        }
+
+        [HttpPost]
+        public async Task<MechanicResponse> UpdateMechanic(
+            string token, int id, string name)
+        {
+            var headers = new Metadata
+            {
+                { "Authorization", $"Bearer {token}" }
+            };
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:40443");
+            var client = new gRPC.Grpc.GrpcClient(channel);
+            var response = await client.UpdateMechanicAsync(
+                new UpdateMechanicRequest { Id = id, Name = name },
+                headers);
+
+            return response;
+        }
     }
 }
--- MyBGList/gRPC/GrpcService.cs
+++ MyBGList/gRPC/GrpcService.cs
@@ -52,5 +52,77 @@ namespace MyBGList.gRPC
 
             return response;
         }
+
+        public override async Task<DomainResponse> GetDomain(
+            DomainRequest request, ServerCallContext scc)
+        {
+            var domain = await _context.Domains
+                .FirstOrDefaultAsync(d => d.Id == request.Id);
+
+            var response = new DomainResponse();
+            if (domain != null)
+            {
+                response.Id = domain.Id;
+                response.Name = domain.Name;
+            }
+
+            return response;
+        }
+
+        [Authorize(Roles = RoleNames.Moderator)]
+        public override async Task<DomainResponse> UpdateDomain(
+            UpdateDomainRequest request, ServerCallContext scc)
+        {
+            var domain = await _context.Domains
+                .FirstOrDefaultAsync(d => d.Id == request.Id);
+
+            var response = new DomainResponse();
+            if (domain != null)
+            {
+                domain.Name = request.Name;
+                _context.Domains.Update(domain);
+                await _context.SaveChangesAsync();
+                response.Id = domain.Id;
+                response.Name = domain.Name;
+            }
+
+            return response;
+        }
+
+        public override async Task<MechanicResponse> GetMechanic(
+            MechanicRequest request, ServerCallContext scc)
+        {
+            var mechanic = await _context.Mechanics
+                .FirstOrDefaultAsync(m => m.Id == request.Id);
+
+            var response = new MechanicResponse();
+            if (mechanic != null)
+            {
+                response.Id = mechanic.Id;
+                response.Name = mechanic.Name;
+            }
+
+            return response;
+        }
+
+        [Authorize(Roles = RoleNames.Moderator)]
+        public override async Task<MechanicResponse> UpdateMechanic(
+            UpdateMechanicRequest request, ServerCallContext scc)
+        {
+            var mechanic = await _context.Mechanics
+                .FirstOrDefaultAsync(m => m.Id == request.Id);
+
+            var response = new MechanicResponse();
+            if (mechanic != null)
+            {
+                mechanic.Name = request.Name;
+                _context.Mechanics.Update(mechanic);
+                await _context.SaveChangesAsync();
+                response.Id = mechanic.Id;
+                response.Name = mechanic.Name;
+            }
+
+            return response;
+        }
     }
 }
