d064d36bd47cec6451cde961e90048ad742f550d
dmitry.sirotkin <d.sirotkin.n@gmail.com>
2024-03-11 11:06:55 +0400

    Add security headers.

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -236,6 +236,21 @@ if (app.Configuration.GetValue<bool>("UseSwagger"))
     app.UseSwaggerUI();
 }
 
+if (!app.Environment.IsDevelopment())
+{
+    // HTTP Security Headers
+    app.UseHsts();
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Append("X-Frame-Options", "sameorigin");
+        context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
+        context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+        context.Response.Headers.Append("Content-Security-Policy", "default-src ' self' ;");
+        context.Response.Headers.Append("Referrer-Policy", "strict-origin");
+        await next();
+    });
+}
+
 if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
     app.UseDeveloperExceptionPage();
 else
